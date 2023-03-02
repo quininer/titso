d3d6f537861595475bed31878ca7c354c913b31b
quininer <quininer@live.com>
2023-03-02 21:07:14 +0800

    cli: fix set

--- cli/src/store.rs
+++ cli/src/store.rs
@@ -1,5 +1,6 @@
-use std::fs;
+use std::{ fs, io };
 use std::path::{ Path, PathBuf };
+use serde::{ Deserialize, Serialize };
 use serde_bytes::Bytes;
 use titso_core::packet;
 
@@ -8,7 +9,14 @@ pub struct Storage {
     config_path: PathBuf
 }
 
-type StoreList<'a> = Vec<(&'a Bytes, &'a Bytes)>;
+const VERSION: u8 = 2;
+
+#[derive(Deserialize, Serialize)]
+struct StoreList<'a> {
+    version: Option<u8>,
+    #[serde(borrow)]
+    list: Vec<(&'a Bytes, &'a Bytes)>
+}
 
 impl Storage {
     pub fn new(config_path: PathBuf) -> Storage {
@@ -19,7 +27,7 @@ impl Storage {
         let mut buf = Vec::new();
         let list = read_list(&self.config_path, &mut buf)?;
 
-        let item = list.iter()
+        let item = list.list.iter()
             .find(|(k, _)| &key.0 == k.as_ref())
             .map(|(_, v)| Vec::from(&***v));
         Ok(item)
@@ -27,10 +35,17 @@ impl Storage {
 
     pub fn set(&self, key: &packet::Tag, value: &[u8]) -> anyhow::Result<()> {
         let mut buf = Vec::new();
-        let mut list = read_list(&self.config_path, &mut buf)?;
+        let mut list = match read_list(&self.config_path, &mut buf) {
+            Ok(list) => list,
+            Err(ref err) if err.downcast_ref::<io::Error>()
+                .filter(|err| err.kind() == io::ErrorKind::NotFound)
+                .is_some()
+                => StoreList::default(),
+            Err(err) => return Err(err.into())
+        };
 
-        list.retain(|(k, _)| &key.0 != k.as_ref());
-        list.push((Bytes::new(&key.0), Bytes::new(value)));
+        list.list.retain(|(k, _)| &key.0 != k.as_ref());
+        list.list.push((Bytes::new(&key.0), Bytes::new(value)));
 
         write_list(&self.config_path, &list)
     }
@@ -39,7 +54,7 @@ impl Storage {
         let mut buf = Vec::new();
         let mut list = read_list(&self.config_path, &mut buf)?;
 
-        list.retain(|(k, _)| &key.0 != k.as_ref());
+        list.list.retain(|(k, _)| &key.0 != k.as_ref());
 
         write_list(&self.config_path, &list)
     }
@@ -51,7 +66,14 @@ fn read_list<'a>(path: &Path, buf: &'a mut Vec<u8>) -> anyhow::Result<StoreList<
     let mut fd = fs::File::open(path.join("titso-storelist.bin"))?;
     buf.clear();
     fd.read_to_end(buf)?;
-    cbor4ii::serde::from_slice(buf.as_slice()).map_err(Into::into)
+
+    let list: StoreList<'_> = cbor4ii::serde::from_slice(buf.as_slice())?;
+
+    if list.version != Some(VERSION) {
+        anyhow::bail!("storelist version not match: {:?}", list.version);
+    }
+
+    Ok(list)
 }
 
 fn write_list(path: &Path, list: &StoreList<'_>) -> anyhow::Result<()> {
@@ -71,4 +93,13 @@ fn write_list(path: &Path, list: &StoreList<'_>) -> anyhow::Result<()> {
     fs::rename(tmppath, path.join("titso-storelist.bin"))?;
 
     Ok(())
+}
+
+impl Default for StoreList<'_> {
+    fn default() -> Self {
+        StoreList {
+            version: Some(VERSION),
+            list: Vec::new()
+        }
+    }
 }
